f44e2fb515199607b6533dedd4f22547953b67c4
OV3rSimplified <twinkelbuggs@gmail.com>
2023-03-27 03:49:13 +0200

    HexShared v.0.0.1
    INIT HexSh

--- hexshared/lua/autorun/sh_lerps.lua
+++ hexshared/lua/autorun/sh_lerps.lua
@@ -1,6 +1,6 @@
 --[[For some I am just a rib, but for others the biggest dream, you can easily but often difficult with me, many do not understand my intentions and do not get along with it, what am I?]]
 if (!HexSh) then return end
-
+ 
 function HexSh:LerpColor( from, to, time )
 
 	local interpolation_data = {
