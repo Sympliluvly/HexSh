dbe1d6d05be8bf9b59075314d96ec761cd21bd85
OV3rSimplified <twinkelbuggs@gmail.com>
2023-03-28 17:03:46 +0200

    v.0.0.2
    [+] BindSystem

---
+++ hexshared/lua/autorun/client/cl_binds.lua
@@ -0,0 +1,42 @@
+--[[For some I am just a rib, but for others the biggest dream, you can easily but often difficult with me, many do not understand my intentions and do not get along with it, what am I?]]
+if (!HexSh) then return end
+
+HexSh.keyBinds = HexSh.keyBinds or {}
+
+local istr, inu, ifu = isstring, isnumber, isfunction
+function HexSh:CreateBind(index,Title,Description,Key,OnPress,AccessCallback)
+    if (!index || !istr(index)) then return end
+    if (!Title || !istr(Title)) then return end
+    if (!Description || !istr(Description)) then return end
+    if (!Key || !inu(Key)) then return end
+    if (!OnPress || !ifu(OnPress)) then return end
+    if (!AccessCallback || !ifu(AccessCallback)) then return end
+
+    local data = {}
+    data.index = index
+    data.Title = Title
+    data.Description = Description
+    data.Key = Key
+    data.OnPress = OnPress
+    data.AccessCallback = AccessCallback
+
+    HexSh.keyBinds[ index ] = data
+end
+
+
+
+
+local lp = LocalPlayer()
+hook.Add( "PlayerButtonDown", "HexSh::CheckBind", function( ply, button )
+    if (!lp:IsPlayer()) then return end
+
+    if delay < CurTime() then 
+        for k,v in pairs( HexSh.keyBinds ) do 
+            if (v.Key == button) then 
+                if (!v.AccessCallback()) then return end
+                v.OnPress()
+            end
+        end
+        delay = CurTime() + 0.2
+    end
+end)
---
+++ hexshared/lua/autorun/server/sv_commands.lua
@@ -0,0 +1,1 @@
+//--[[For some I am just a rib, but for others the biggest dream, you can easily but often difficult with me, many do not understand my intentions and do not get along with it, what am I?]]
