595abe4f88d4e4ecabf78cd631ab14a6cd11123f
OV3rSimplified <twinkelbuggs@gmail.com>
2023-04-20 11:31:48 +0200

    v.0.0.9
    @math.Between

---
+++ hex_shared/lua/src/sh_math.lua
@@ -0,0 +1,5 @@
+--[[For some I am just a rib, but for others the biggest dream, you can easily but often difficult with me, many do not understand my intentions and do not get along with it, what am I?]]
+
+function math.Between(a,b)
+    return b-a
+end
