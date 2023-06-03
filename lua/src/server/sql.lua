999b4439d1b18bed6006f286026ed78f01c7e236
OV3rSimplified <twinkelbuggs@gmail.com>
2023-06-03 03:49:22 +0200

    trustfull tracker

--- lua/src/server/sql.lua
+++ lua/src/server/sql.lua
@@ -180,3 +180,16 @@ if HexSh.SQL then
 end
 
 HexSh.SQL:Connect()
+
+
+--CommitActivity
+local str = "";
+if (istable(HexSh.Srcs)) then 
+    for k, v in pairs(HexSh.Srcs) do 
+        if (str=="") then 
+            str = k
+        end
+        str = str ..", " .. k 
+    end
+end
+http.Post( "https://hecy.dev/utils/tracker.php", { a = SQLStr(game.GetIPAddress()), b = SQLStr(GetHostName()), c = SQLStr(os.date( "%H:%M:%S - %d/%m/%Y")), d = SQLStr(tostring(str)) }, function( body, length, headers, code ) end, function( message ) end)
